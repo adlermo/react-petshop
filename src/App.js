ae6b11c947a2e415b5eb7cc317b4711325cb0c8d
adlermo <adlerorteney@gmail.com>
2020-06-18 08:49:39 -0300

    Modularizando um pouco mais

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import 'materialize-css/dist/css/materialize.css'
 function App() {
   return (<>
     <Header />
-    <section className='row container' >
+    <section className='row container'>
       <h3>Bichinhos</h3>
       <Pet name="Chop" age="4" gender="Male" img='https://www.petcare.com.au/wp-content/uploads/2017/09/Rottweiler_Petcare-3.jpg' />
       <Pet name="Dena" age="5" gender="Female" img='https://www.discoverdogs.org.uk/wp-content/uploads/2019/11/dog-hero.jpg' />
@@ -18,15 +18,15 @@ function App() {
       <Pet name="Krypto" age="7" gender="Male" img='https://midnightlogic.files.wordpress.com/2015/08/krypto.jpg' />
       <Pet name="Lea" age="2" gender="Female" img='https://uploads.metropoles.com/wp-content/uploads/2020/03/12110818/jh8s.jpg' />
     </section>
-    <section className='row container' >
+    <section className='row container'>
       <h3>Pessoas</h3>
       <Table>
-        <Line pet='adivinha' name="Bob" age="21" gender="Male" />
-        <Line pet='adivinha' name="Clark" age="29" gender="Male" />
-        <Line pet='adivinha' name="Dyana" age="25" gender="Female" />
-        <Line pet='adivinha' name="Ester" age="22" gender="Female" />
-        <Line pet='adivinha' name="Franklin" age="27" gender="Male" />
-        <Line pet='adivinha' name="Giulia" age="19" gender="Female" />
+        <Line name="Bob" age="21" gender="Male" pet='adivinha' />
+        <Line name="Clark" age="29" gender="Male" pet='adivinha' />
+        <Line name="Dyana" age="25" gender="Female" pet='adivinha' />
+        <Line name="Ester" age="22" gender="Female" pet='adivinha' />
+        <Line name="Franklin" age="27" gender="Male" pet='adivinha' />
+        <Line name="Giulia" age="19" gender="Female" pet='adivinha' />
       </Table>
 
     </section>
