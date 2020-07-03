06674afdb83bf05e308e3337ad5c08a4f9a3e11f
Adler Mesquita Orteney <adlerorteney@gmail.com>
2020-07-03 12:11:50 -0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Calendar Image
+// https://www.papertraildesign.com/wp-content/uploads/2019/08/2020-Calendar.jpg  .HUGE
+// https://cdn1.vectorstock.com/i/1000x1000/65/35/2020-calendar-year-template-planner-vector-26296535.jpg  .PRETTY
+
 const Header = (props) => {
     return (
         <nav>
